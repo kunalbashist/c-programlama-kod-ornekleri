540bd15e01aea6e644750d0437fe0e99f84267f6
kunalbashist <62015640+kunalbashist@users.noreply.github.com>
2020-10-01 00:04:08 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -3,31 +3,16 @@ Fibonacci serisini bulan program.
 */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-void fibonacci(int);
-
-int main()
-{
-    fibonacci(10);
-
-    return 0;
-}
-
-void fibonacci(int sayi)
-{
-    int a = 0, b = 1, tasiyici, i;
-
-    printf("%d  ", a);
-    printf("%d  ", b);
-
-    for(i = 0; i < sayi - 2; i++)
-    {
-        printf("%d  ", a + b);
-        tasiyici = a;
-        a = b;
-        b = tasiyici + b;
-    }
-
-    printf("\n");
-}
+int fib(int n) 
+{ 
+    if (n <= 1) 
+        return n; 
+    return fib(n - 1) + fib(n - 2); 
+} 
+  
+int main() 
+{ 
+    int n = 9; 
+    printf("%d", fib(n));  
+    return 0; 
+} 
